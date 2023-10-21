d934d87b8d265550072c2922fceef28353081ca2
Mohamed M. Kosba - محمدمحمودكسبه <mohamed.kosba96@gmail.com>
2023-10-21 11:21:25 +0400

    2- (Updated) Demonstrate asynchronous generator with bloc including with transformers

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '1_builder_pattern/real_estate_property_view.dart';
-// import '2_bloc_generator_with_transformers/view/posts_view.dart';
+// import '2_bloc_generator_with_transformers/view/generator_with_transformars_example.dart';
 import '3_render_objects_with_custom_paints/smiley_face_view.dart';
 import '4_bouncing_sticky_header_with_infinite_list/list_with_sticky_header_view.dart';
 import '5_retry_policy/retry_policy_view.dart';
